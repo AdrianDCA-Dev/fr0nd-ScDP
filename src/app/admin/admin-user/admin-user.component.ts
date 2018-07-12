638b8230a4f0a4a2411391c882a8d7a9e68eae0f
AdrianDCA-Dev <adriandca7@gmail.com>
2018-07-12 12:33:10 -0400

    update user, post defensa

--- src/app/admin/admin-user/admin-user.component.ts
+++ src/app/admin/admin-user/admin-user.component.ts
@@ -63,6 +63,7 @@ export class AdminUserComponent implements OnInit {
         direccion: [null, Validators.required],
         telefono: [null, [Validators.required, Validators.maxLength(10), Validators.minLength(5)]],
         celular: [null, [Validators.required, Validators.maxLength(10), Validators.minLength(5)]],
+        facultad: [null, [Validators.required]],
         carrera_id: [null, [Validators.required]],
         name: [null, [Validators.required, Validators.maxLength(50), Validators.minLength(4)]],
         email: [null, [Validators.required, Validators.maxLength(100), Validators.email]],
@@ -82,6 +83,7 @@ export class AdminUserComponent implements OnInit {
         direccion: [null, Validators.required],
         telefono: [null, [Validators.required, Validators.maxLength(10), Validators.minLength(5)]],
         celular: [null, [Validators.required, Validators.maxLength(10), Validators.minLength(5)]],
+        facultad: [null, [Validators.required]],
         carrera_id: [null, [Validators.required]],
         name: [null, [Validators.required, Validators.maxLength(50), Validators.minLength(4)]],
         email: [null, [Validators.required, Validators.maxLength(100), Validators.email]],
@@ -89,9 +91,30 @@ export class AdminUserComponent implements OnInit {
         password_confirmation: [null, [Validators.required, Validators.maxLength(60), Validators.minLength(8)]],
         estado: [null, Validators.required],
         role: [null, Validators.required],
+        length: [null, Validators.required],
     });
   }
+  show(formData) {
+    console.log('formData', formData);
+    console.log('fromSexo', formData.persona.sexo);
 
+    this.id = formData.id;
+    this.editForm.controls['id'].setValue(formData.persona.id);
+    this.editForm.controls['ci'].setValue(formData.persona.ci);
+    this.editForm.controls['nombre'].setValue(formData.persona.nombre);
+    this.editForm.controls['apellidos'].setValue(formData.persona.apellidos);
+    this.editForm.controls['sexo'].setValue(formData.persona.sexo);
+    this.editForm.controls['fechaNac'].setValue(formData.persona.fechaNac);
+    this.editForm.controls['direccion'].setValue(formData.persona.direccion);
+    this.editForm.controls['telefono'].setValue(formData.persona.telefono);
+    this.editForm.controls['celular'].setValue(formData.persona.celular);
+    this.editForm.controls['carrera_id'].setValue(formData.persona.carrera_id);
+    this.editForm.controls['name'].setValue(formData.name);
+    this.editForm.controls['email'].setValue(formData.email);
+    this.editForm.controls['password'].setValue(formData.password);
+    this.editForm.controls['password_confirmation'].setValue(formData.password_confirmation);
+    this.editForm.controls['estado'].setValue(formData.estado);
+  }
   post(model: any) {
     console.log('funiona', model);
     this.userService.postUsers(model).subscribe(data => {
@@ -101,6 +124,29 @@ export class AdminUserComponent implements OnInit {
       swal('¡Buen trabajo!', '¡Hiciste clic en el botón!', 'success');
     });
     this.myForm.reset();
+  }
+
+  edit(model: any) {
+    const formValue = this.editForm.getRawValue();
+    const index = this.data.findIndex( data => data.id == formValue.id);
+    this.model = model;
+    this.model.id = this.id;
+    this.userService.putUsers(this.model.id, this.model).subscribe( data => {
+      console.log(data);
+      console.log('formValue', this.data[index]);
+      this.data[index].persona.ci = formValue.ci;
+      this.data[index].persona.nombre = formValue.nombre;
+      this.data[index].persona.apellidos = formValue.apellidos;
+      this.data[index].persona.fechaNac = formValue.fechaNac;
+      this.data[index].persona.direccion = formValue.direccion;
+      this.data[index].persona.telefono = formValue.telefono;
+      this.data[index].persona.celular = formValue.celular;
+      this.data[index].name = formValue.name;
+      this.data[index].email = formValue.email;
+      $('#myModalEdit').modal('hide');
+      swal('¡Buen trabajo!', '¡Hiciste clic en el botón!', 'success');
+    });
+    this.editForm.reset();
   }
 
   randomPassword(length: any) {
@@ -118,6 +164,12 @@ export class AdminUserComponent implements OnInit {
     const gen = this.randomPassword(10);
     this.myForm.controls['password'].setValue(gen);
     this.myForm.controls['password_confirmation'].setValue(gen);
+    console.log('password', this.randomPassword(10));
+  }
+  generarm() {
+    const gen = this.randomPassword(10);
+    this.editForm.controls['password'].setValue(gen);
+    this.editForm.controls['password_confirmation'].setValue(gen);
     console.log('password', this.randomPassword(10));
   }
   carrera(id: any) {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
+/*
   api_url: 'https://back-seg-mod.herokuapp.com/api'
-  /*api_url: 'http://localhost:8000/api'*/
+*/
+  api_url: 'http://localhost:8000/api'
 };
